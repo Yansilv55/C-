f3b2124945285c8fd7ca379bb38bad97e7d561ab
Yansilv55 <yansilv55@gmail.com>
2023-09-27 10:29:03 -0300

    type cast

---
+++ aula11/aula11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            int n2 = n1;
+
+            Console.WriteLine(n2);
+
+            Console.ReadLine();
+        }
+    }
+}
