10aa8f01243be1ea7992f225f3145e9bf1cd73b1
Yansilv55 <yansilv55@gmail.com>
2023-10-18 09:06:38 -0300

    commit

--- aula15/aula15/Program.cs
+++ aula15/aula15/Program.cs
@@ -4,16 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace aula14
+namespace aula15
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] n = new int[5];
-            int[] num = new int[4]{33, 23, 65, 34};
-
-            n[0] = 22;
         }
     }
 }
