44f4bf67b5eb4cca7c190dc76942dfab032e0fa5
Yansilv55 <yansilv55@gmail.com>
2023-10-26 10:44:33 -0300

    do while

--- aula16/aula16/Program.cs
+++ aula16/aula16/Program.cs
@@ -18,27 +18,8 @@ namespace aula16
             }
             Console.WriteLine("End");
 
-         
-        }
-
-        static void body(string[] args)
-        {
-            string senha = "123";
-            string senhauser;
-            int tentativas = 0;
-
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("Digite sua senha:");
-                senhauser = Console.ReadLine();
-
-            }while(senha != senhauser);
-
-            Console.WriteLine("Senha correta, tentativas:{0}", tentativas);
-
             Console.ReadLine();
-
         }
+     
     }
 }
---
+++ aula17/aula17/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123";
+            string senhauser;
+            int tentativas = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Digite sua senha:");
+                senhauser = Console.ReadLine();
+                tentativas++;
+
+            } while (senha != senhauser);
+
+            Console.Clear();
+            Console.WriteLine("Senha correta, tentativas:{0}", tentativas);
+
+            Console.ReadLine();
+        }
+    }
+}
