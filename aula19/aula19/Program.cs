2c961004382d9b9d63e5be284ac9e2c0aa7b77f2
Yansilv55 <yansilv55@gmail.com>
2023-12-27 05:05:12 -0300

    Arrays

---
+++ aula19/aula19/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] linguagens = new string[4];
+            int[] anoslinguagem = new int[4];
+
+            linguagens[0] = "JavaScript";
+            linguagens[1] = "Cshap";
+            linguagens[2] = "Java";
+            linguagens[3] = "PHP";
+
+            anoslinguagem[0] = 1990;
+            anoslinguagem[1] = 1984;
+            anoslinguagem[2] = 1994;
+            anoslinguagem[3] = 1964;
+
+            Console.WriteLine($"Linguagens de programação 1: {linguagens[2]} foi criada no ano de {anoslinguagem[2]}");
+
+            Console.ReadLine();
+        }
+    }
+}
