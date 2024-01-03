c7448f63f0823c0242be1d07173f180a8954332f
Yansilv55 <yansilv55@gmail.com>
2024-01-03 17:43:28 -0300

    commit

---
+++ aula20/aula20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            dobrar(num);
+            Console.WriteLine(num);
+
+            Console.ReadLine();
+        }
+        static void dobrar(int valor)
+        {
+            valor*= 2;
+
+            Console.ReadLine();
+        }
+    }
+}
