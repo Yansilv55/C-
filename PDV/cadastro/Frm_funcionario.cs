5b2fd854e8ba7c0cee9ff0c2a871952449b9c1d3
Yansilv55 <yansilv55@gmail.com>
2024-01-19 11:42:04 -0300

    PDV

--- PDV/cadastro/Frm_funcionario.cs
+++ PDV/cadastro/Frm_funcionario.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,9 @@ namespace PDV.cadastro
 {
     public partial class Frm_funcionario : Form
     {
+        Conexao con = new Conexao();
+        string sql = string.Empty;
+        MySqlCommand banco;
         public Frm_funcionario()
         {
             InitializeComponent();
@@ -25,7 +30,18 @@ namespace PDV.cadastro
 
         private void btn_Salvar_Click(object sender, EventArgs e)
         {
+            con.AbrirConexao();
+            sql = "SELECT * FROM `funcionarios`";
+            banco = new MySqlCommand(sql, con.con);
 
+            using (MySqlDataReader reader = banco.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    string linha = reader.Read().ToString();
+
+                }
+            }
         }
     }
 }
