6f31dfa7e32b735ae55708e5e3355521097179b4
Yansilv55 <yansilv55@gmail.com>
2024-05-27 16:34:23 -0300

    commit

---
+++ Csharp POO/POO 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class POO1
+{
+	public POO1()
+	
+	{
+		static void main(string[] args)
+		{
+			console.WriteLine("Hello Word!");
+		}
+
+	
+	}
+		public interface Ipagamento
+		{
+		    Datetime Vencimento {  get; set; }
+		    decimal Valor { get; set; }
+		}
+
+	    public class Pagamento : Ipagamento
+		{
+
+		}
+}
